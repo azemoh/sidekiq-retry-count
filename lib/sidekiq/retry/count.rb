0062381a6b94c09c2249d5b46872740193c83b25
Joshua Azemoh <joshazemoh@gmail.com>
2023-05-30 15:06:32 -0400

    Initial Commit

---
+++ lib/sidekiq/retry/count.rb
@@ -0,0 +1,10 @@
+require "sidekiq/retry/count/version"
+
+module Sidekiq
+  module Retry
+    module Count
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
